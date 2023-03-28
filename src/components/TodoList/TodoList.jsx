e9d5a8422852ef045d7c5d41c84fc325c4ef849c
soyeon <thdus970130@naver.com>
2023-03-28 21:02:27 +0900

    Save ToDoList

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,16 +4,18 @@ import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css';
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    { id: '123', text: '장보기', status: 'active' },
-    { id: '124', text: '공부하기', status: 'active' },
-  ]);
+  const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
+  // useState의 초깃 값을 일반함수로 주게 되면 계속 내부적으로 리렌더링 된다.
+  // 이를 방지하기 위해 초깃값을 콜백함수로 넣어주면 된다.
   const handleAdd = (todo) => setTodos([...todos, todo]);
   const handleUpdate = (updated) =>
     setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
   const handleDelete = (deleted) =>
     setTodos(todos.filter((t) => t.id !== deleted.id));
 
+    useEffect(()=>{
+      localStorage.setItem('todos',JSON.stringify(todos))
+    }, [todos]);
   const filtered = getFilteredItems(todos, filter);
   return (
     <section className={styles.container}>
@@ -31,7 +33,10 @@ export default function TodoList({ filter }) {
     </section>
   );
 }
-
+function readTodosFromLocalStorage(){
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
+}
 function getFilteredItems(todos, filter) {
   if (filter === 'all') {
     return todos;
